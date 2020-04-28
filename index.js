e74a5d3bce7e3ee447f8035082ae5f8769689858
Joseph Rivera <josephr98@gmail.com>
2020-04-28 17:22:38 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
 const movieDataSmall = require('./movie-data-small');
+
+app.use(cors());
+app.use(morgan("common"));
+app.use(helmet());
+
+app.use(validateBearerToken);
+
+const API_TOKEN = process.env.API_TOKEN;
+
+function validateBearerToken(req, res, next) {
+    const { authorization } = req.headers;
+    if (!authorization) {
+        res.status(401).json({error: "You have to include your Auth token"})
+    }
+    if (authorization) {
+        let validAuth = authorization.split(' ')[1];
+        if (validAuth !== API_TOKEN) {
+            res.status(400).json({error: "You provided the wrong Auth token"})
+        }
+        if (validAuth === API_TOKEN) {
+            next(); 
+        }
+    }
+    
+}
+
+
 
 
 app.get('/movies', (request, response) => {
     const {genre, country, averageVote} = request.query;
+
     let correctGenres = [];
     if(genre){
-    movieDataSmall.map(movie => {
-        if (genre.toLowerCase() === movie.genre.toLowerCase()){ return correctGenres.push(movie)};
+        correctGenres = movieDataSmall.filter(movie => {
+            return movie.genre.toLowerCase().includes(genre.toLowerCase());
         });
-        return genre;
     }
-    response.json({correctGenres});
+
+    let correctCountry = [];
+    if(country){
+        correctCountry = movieDataSmall.filter(movie => {
+            return movie.country.toLowerCase().includes(country.toLowerCase());
+        });
+    }
+
+    let correctVote = [];
+    parseFloat(averageVote);
+    if(averageVote){
+        correctVote = movieDataSmall.filter(movie => {
+            return movie.avg_vote > averageVote;
+        });
+    }
+
+    let masterArray = [ ...correctGenres, ...correctCountry, ...correctVote ];
+
+    response.json({masterArray});
 });
-// q ? genre, country, average vote
+
+
 app.listen(8080, () => console.log('Server listening on port 8080...'));
