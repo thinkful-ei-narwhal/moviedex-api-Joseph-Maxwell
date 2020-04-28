11143529e6af8a02d9383059e572d377491595c9
MxJRd <reed182@mail.chapman.edu>
2020-04-28 12:43:36 -0700

    ,m

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+'use strict';
 
+const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const cors = require('cors');
+const movieDataSmall = require('./movie-data-small');
 
 
-app.listen(8080, () => console.log('Server listening on port 8080...'))
+app.get('/movies', (request, response) => {
+    const {genre, country, averageVote} = request.query;
+    let correctGenres = [];
+    if(genre){
+    movieDataSmall.map(movie => {
+        if (genre.toLowerCase() === movie.genre.toLowerCase()){ return correctGenres.push(movie)};
+        });
+        return genre;
+    }
+    response.json({correctGenres});
+});
+// q ? genre, country, average vote
+app.listen(8080, () => console.log('Server listening on port 8080...'));
